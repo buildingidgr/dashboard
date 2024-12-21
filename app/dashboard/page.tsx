d14d9c7b80a27091b8c1d566cf75165098276a91
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2024-12-21 11:54:57 +0200

    sdf

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -166,7 +166,6 @@ export default function Page() {
             backgroundImage="https://images.unsplash.com/photo-1581091878330-f5cf2a3b1564?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
             </div>
-            <ArticlesCreated/>
           </div>
           </div>
           <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
